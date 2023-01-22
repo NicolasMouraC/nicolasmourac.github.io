5088155084f0d3d5b18614c0ddad573193aeb9ed
Nicolas <nicolasoutrog@gmail.com>
2023-01-22 19:25:50 -0300

    Add header and skills container

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,7 +5,7 @@ import "../Animation.css"
 
 const MainContainer = () => {
     return (
-            <main className="pt-8 pl-14 pr-16 pb-1 bg-slate-800 h-screen relative md:pl-24">
+            <main className="pt-8 pl-10 pr-10 pb-1 bg-slate-800 h-screen relative md:pl-24">
                 <div className="background">
                     <li></li>
                     <li></li>
@@ -19,7 +19,7 @@ const MainContainer = () => {
                 </div>
                 <section>
                     <div className="absolute bottom-[50px]">
-                        <header className="text-gray-100 text-[40px] my-[20px] flex flex-wrap md:text-[60px]">
+                        <header className="text-gray-100 text-[40px] font-bold my-[20px] flex flex-wrap md:text-[60px]">
                             <h1 className="mr-2">Hi,</h1>
                             <h1 className="mr-2">I'm</h1>
                             <h1 className="mr-2">Nicolas</h1>
@@ -29,19 +29,19 @@ const MainContainer = () => {
                             <h1 className="mr-2 underline decoration-sky-400 underline-offset-2 decoration-8">Developer</h1>
                         </header>
                         <div className="flex space-x-4 my-[20px]">
-                            <a className="text-sky-400 text-[30px] transition ease-linear delay-150 hover:text-stone-100 hover:scale-150" >
+                            <a href="https://github.com/NicolasMouraC" className="text-sky-400 text-[30px] transition ease-linear delay-150 hover:text-stone-100 hover:scale-150" >
                                 <BsGithub/>
                             </a>
-                            <a className="text-sky-400 text-[30px] transition ease-linear delay-150 hover:text-stone-100 hover:scale-150" >
+                            <a href="https://www.linkedin.com/in/nicolas-moura-b677b8232/" className="text-sky-400 text-[30px] transition ease-linear delay-150 hover:text-stone-100 hover:scale-150" >
                                 <BsLinkedin/>
                             </a>
-                            <a className="text-sky-400 text-[30px] transition ease-linear delay-150 hover:text-stone-100 hover:scale-150">
+                            <a href="mailto:nicolasoutrog@gmail.com" className="text-sky-400 text-[30px] transition ease-linear delay-150 hover:text-stone-100 hover:scale-150">
                                 <MdEmail/>
                             </a>
                         </div>
                         <article className="w-[400px] text-[20px] md:text-[25px]">
                             <p className="text-gray-300">
-                                I love exploring and creating 🚀 I'm a lifelong learner 🎓 and I might have a thing for traditional Neapolitan Pizza 🍕
+                                I love to explore new things and possibilities 🚀 I'm a lifelong learner 🎓 Disciplined as a military 🎖️ and maybe I have a thing for weightlifting 💪
                             </p>
                         </article>
                     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { AiFillStar } from 'react-icons/ai';
+import Photo from './images/myPhoto.jpeg';
 
 const Skills = () => {
     return (
-        <section className="grid">
-
+        <section>
+            <div className="text-[40px] font-medium mb-[20px] md:text-[60px]">My <span className="underline decoration-sky-400 underline-offset-2 decoration-8">Skills</span></div>
+            <div className="grid gap-[40px] grid-cols-1 md:grid-cols-4">
+                <img 
+                    src={Photo}
+                    className="max-h-[400px]"
+                    alt="Nicolas Picture">
+                </img>
+                <article className="md: md:col-start-2 md:col-span-3">
+                    <h2 className="text-[26px] font-medium text-sky-400 flex md:text-[30px]">
+                        Always Dreaming Big
+                        <AiFillStar color="yellow"/>
+                    </h2>
+                    <p className="text-[20px] font-light md:text-[24px]">
+                        I always had interest in tech. My first contact with programming was when i was 12 years-old.
+                        Since then, I have been practicing various knowledge related to technology in general. 
+                        Lately I discovered the magic and possibilities of web development. 
+                        Whenever I can, I always seek new knowledge and skills related to it.
+                    </p>
+                    <button className="button">See more about my personal life</button>
+                </article>
+            </div>
         </section>
     )
 }
