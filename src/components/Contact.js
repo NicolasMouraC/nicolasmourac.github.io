94dd17723eaa52d31268bc9782970385433a2d4f
Nicolas de Moura <97635109+NicolasMouraC@users.noreply.github.com>
2023-03-20 19:35:59 -0300

    Update phone number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
             <div className="grid grid-cols-2">
                 <div>
                     <h2 className="font-medium text-[40px] md:text-[60px] mb-[10px]">Contact Me</h2>
-                    <h2 className="mb-[10px] text-sky-600 font-semibold">Call me: +55 (99) 98520-3232</h2>
+                    <h2 className="mb-[10px] text-sky-600 font-semibold">Call me: +55 (11) 96607-7935</h2>
                     <a href="mailto:nicolasoutrog@gmail.com" className="button">Send me an email</a>
                 </div>
                 <img srcset={astronautContact} alt="By catalyststuff on Freepik" className="max-h-[200px]"/>
